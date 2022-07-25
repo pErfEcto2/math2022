d3e8fa79d9642259b1c3246d995149b102a31142
perfecto <misha.yaro2004@gmail.com>
2022-07-25 10:46:35 +0300

    Algorithm to find min of a function added

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 
 
 import lib
+from time import time
 
 
 #file = "src/data.txt" # 0 1 1 0
@@ -11,7 +12,6 @@ import lib
 #file = "src/matrix4.txt" # 1 1 1
 #file = "src/infinite_solutions.txt" # exception "Infinite number of solutions"
 #file = "src/no_solutions.txt" # exception "No solutions"
-
 
 #with open(file, "r") as f:
 #    data = [list(map(int, s.split())) for s in f.readlines()]
@@ -32,23 +32,56 @@ import lib
 # 
 # print(f"y = {ans[0]}*x^2 + {ans[1]}*x + {ans[2]}")
 
+
+
+
+
 start, finish = list(map(float, input("$ ").split()))
 
-if lib.f(start) * lib.f(finish) >= 0 or start >= finish:
+if start >= finish:
     raise Exception
 
-x = finish - ((finish - start) * lib.f(finish) / (lib.f(finish) - lib.f(start)))
+# X = 1e-3
 
-accurance = 1e-14
+x1 = start * (1 + (3 - 5 ** 0.5) / 2)
+x2 = start * (1 - (5 ** 0.5 - 1) / 2)
 
-while abs(lib.f(x)) > accurance:
-    x = finish - ((finish - start) * lib.f(finish) / (lib.f(finish) - lib.f(start)))
+y1 = lib.f(x1)
+y2 = lib.f(x2)
 
-    if x < 0:
-        finish = x
+fStart = lib.f(start)
+fFinish = lib.f(finish)
+
+# x = finish - ((finish - start) * lib.f(finish) / (lib.f(finish) - lib.f(start)))
+# 
+# x = start
+# x = (lib.f(x + dX) - 2 * lib.f(x) + lib.f(x - dX)) / (dX ** 2)
+
+accuracy = 1e-10
+t = time()
+while abs(x1 - x2) >= accuracy <= abs(start - finish):
+    if fStart >= y1 >= y2:
+        start, x1 = x1, x2
+        fStart, y1 = y1, y2
+        x2 = (1 + (3 - 5 ** 0.5) / 2) * start
+        y2 = lib.f(x2)
+
     else:
-        start = x
+        finish, x2 = x2, x1
+        fFinish, y2 = y2, y1
+        x1 = (1 - (5 ** 0.5 - 1) / 2) * finish
+        y1 = lib.f(x1)
     
-    # print(f"{start = }; {finish = }; {x = }")
+    # x = finish - ((finish - start) * lib.f(finish) / (lib.f(finish) - lib.f(start)))
 
-print(f"{x = }")
+    # x = (lib.f(x + dX) - 2 * lib.f(x) + lib.f(x - dX)) / (dX ** 2)
+
+    # if x < 0:
+    #     finish = x
+    # else:
+    #     start = x
+    # print(f"{start = }; {finish = }; {x1 = }; {x2 = }; {y1 = }; {y2 = }")
+
+print(f"{min(fStart, y1, y2, fFinish)}")
+print(f"{time() - t}")
+
